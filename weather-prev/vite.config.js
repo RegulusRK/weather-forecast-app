7207e0a8fbd884be50a16c5a7c56559330f265fc
Rafael <136873880+RegulusRK@users.noreply.github.com>
2025-04-24 22:38:22 -0300

    add: icon

--- weather-prev/vite.config.js
+++ weather-prev/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weather-forecast-app/', // <- nome exato do repositório!
   plugins: [react()],
 })
